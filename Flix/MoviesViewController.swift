d3df72e60049dfaa3a98d60b28b2718ecf52480d
Surbhi  Hajela <surbhihajela@Surbhis-MacBook-Pro-2.local>
2020-09-25 21:26:46 -0700

    Initial Commit

---
+++ Flix/MoviesViewController.swift
@@ -0,0 +1,31 @@
+//
+//  MoviesViewController.swift
+//  Flix
+//
+//  Created by Surbhi  Hajela on 9/25/20.
+//  Copyright © 2020 Surbhi  Hajela. All rights reserved.
+//
+
+import UIKit
+import AlamofireImage
+
+class MoviesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
